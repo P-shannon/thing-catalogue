a40a5987d91f89e34a0cac5a9731d4e8fd770252
Patrick Shannon <patrickshannon05021993@gmail.com>
2017-12-12 14:26:16 -0500

    Adds auth funcitonality to the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,40 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	def sign_in(user)
+		token = SecrueRandom.url_base64
+		session[:session_token] = token
+		user.update_attribute(:session_token, token)
+	end
+
+	def current_user
+		token = session[:session_token]
+		token && User.find_by(session_token: token)
+	end
+
+	def current_user
+		@current_user ||= find_current_user
+	end
+
+	def find_current_user
+		token = session[:session_toen]
+		token && User.find_by(session_token: token)
+	end
+
+	def sign_out
+		session.delete(:session_token)
+		current_user.update_attribute(:session_token, nil)
+	end
+
+	def ensure_signed_in
+		return if current_user
+		flash[:error] = 'You must be signed in to see this.'
+		redirect_to #somewhere...
+	end
+
+	def ensure_signed_out
+		return unless current_user
+		flash[:error] = 'You are signed in, so you shouldn\' be here...'
+		redirect_to #somewhere else...
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+	def new
+		@user = User.new
+		render :new
+	end
+
+	def create
+		username = params[:user][:username]
+		password = params[:user][:password]
+
+		user = User.find_from_credentials(username, password)
+
+		if user
+			sign_in(user)
+			flash[:notice] = "Hi, #{username}! You signed in!"
+			redirect_to #somewhere...
+		else
+			flash[:error] = 'Username or password incorrect'
+			@user = User.new(username: username)
+			render :new
+		end
+	end
+
+	def destroy
+		sign_out
+		flash[:notice] = 'You signed out!'
+		redirect_to #somewhere else...
+	end
+
+	
 end
